3b6a8523d6707e5be9fc5299e742204d29cdb6aa
gashaw mucheye <gashaw2533@gmail.com>
2023-09-03 14:34:15 +0200

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'https://mern-stack-5dxo.onrender.com/',
 
         changeOrigin: true,
       },
